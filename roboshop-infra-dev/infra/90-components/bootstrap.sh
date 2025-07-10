a67083e870226b65a8cc2219b985382ce9fba65d
Kiran <kirann.bobby@gmail.com>
2025-07-10 19:28:22 +0530

    Automated VPC Infrastructure Setup using Terraform_Module

--- roboshop-infra-dev/infra/90-components/bootstrap.sh
+++ roboshop-infra-dev/infra/90-components/bootstrap.sh
@@ -3,4 +3,16 @@
 component=$1
 env=$2
 dnf install ansible -y
-ansible-pull -U https://github.com/daws-84s/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
+ansible-pull -U https://github.com/kkumar-2655/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 roles/main.yaml
+
+# #!/bin/bash
+# set -xe  # Debug mode: fail fast and print each command
+
+# component=$1
+# env=$2
+
+# # Install Ansible
+# dnf install ansible -y
+
+# # Pull Ansible roles from GitHub
+# ansible-pull -U https://github.com/kkumar-2655/ansible-roboshop-roles-tf.git -e component=$component -e env=$env main.yaml
